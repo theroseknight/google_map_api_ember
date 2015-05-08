2ce65640e117205a6db1608c6704ad2c8e9bedee
theroseknight <daniel.j.x.bryant@gmail.com>
2015-05-08 14:55:37 -0400

    got a working demo map!

---
+++ app/adapters/application.js
@@ -0,0 +1,26 @@
+import Ember from "ember";
+import DS from "ember-data";
+import ENV from '../config/environment';
+ if (ENV.environment === 'development') {
+   var host='http://localhost:3000'
+ }else{
+   var host='https://toolbelt.kaufmanrossin.com'
+ }
+  Ember.$.ajaxSetup({
+    xhrFields: {
+      withCredentials: true
+    }
+  });
+export default DS.ActiveModelAdapter.extend({
+  headers: {
+    "X-CSRF-Token": Ember.$('meta[name="csrf-token"]').attr('content')
+  },
+  host: host,
+  namespace: 'api/printing',
+  ajax: function(url, method, hash) {
+    hash = hash || {};
+    hash.crossDomain = true;
+    hash.xhrFields = {withCredentials: true};
+    return this._super(url, method, hash);
+  }
+});
---
+++ app/routes/application.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import ENV from '../config/environment';
+export default Ember.Route.extend({
+  setupController:function(controller){
+		this._super();
+		controller.set("screenWidth",screen.width);
+		controller.set("screenHeight",screen.height);
+    controller.set("alertMessage","Welcome to the app!");
+    controller.set("alertClass","alert-success");
+    if(ENV.environment==="production"){
+      //controller.set("baseUrl","https://toolbelt.kaufmanrossin.com/api/printing/")
+      //controller.set("baseWebsocketUrl","wss://toolbelt.kaufmanrossin.com/api/printing/")
+    }else{
+      controller.set("baseUrl","http://localhost:3000")
+      //controller.set("baseWebsocketUrl","ws://localhost:3000/api/printing/")
+    }
+	},
+  actions:{
+    resetMapLocation:function(){
+      window.location.reload(true);
+    }
+  }
+});
