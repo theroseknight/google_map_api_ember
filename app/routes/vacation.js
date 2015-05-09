baccb62f18b33a8814a5a5fea1e7b7fc1d84d2b9
theroseknight <daniel.j.x.bryant@gmail.com>
2015-05-09 15:52:57 -0400

    vacation subresource set up

--- app/routes/vacation.js
+++ app/routes/vacation.js
@@ -3,18 +3,6 @@ export default Ember.Route.extend({
   model: function(params) {
     return this.store.find("vacation",params.vacationId);
 	},
-  setupController:function(controller){
-    this._super();
-    controller.set(
-      'actionsArray',
-      [
-        {action:"resetMap", label:"Reset Map"},
-        {link:"vacations", label:"Return Home"},
-        {action:"addLeg", label:"Add Leg"},
-        {action:"deleteVacation", label:"Delete"}
-      ]
-    );
-  },
   actions:{
 
   }
--- app/routes/vacation/index.js
+++ app/routes/vacation/index.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 export default Ember.Route.extend({
+  model: function() {
+	 return this.store.find("vacation");
+	},
   setupController:function(controller){
     this._super();
     controller.set(
